0eebc7c53dfb1086d95db062a5c23fadcd87a50e
Kathy Nguyen <kathyn262@gmail.com>
2019-11-07 10:38:14 -0800

    changed image size

--- app.py
+++ app.py
@@ -146,8 +146,8 @@ def users_show(user_id):
 
     user = User.query.get_or_404(user_id)
 
-    # snagging messages in order from the database;
-    # user.messages won't be in order by default
+    # need to specify order bc
+    # user.messages isn't in order by default
     messages = (Message
                 .query
                 .filter(Message.user_id == user_id)
