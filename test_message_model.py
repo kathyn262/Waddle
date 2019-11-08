09741843ce4e534e4a19a8950cc33a8625e41ee6
Kathy Nguyen <kathyn262@gmail.com>
2019-11-08 10:51:22 -0800

    added teardown

--- test_message_model.py
+++ test_message_model.py
@@ -1,7 +1,6 @@
 import os
 from unittest import TestCase
-from sqlalchemy.exc import IntegrityError
-from models import db, User, Message, Follows, bcrypt
+from models import db, User, Message, Follows
 from datetime import datetime
 
 # BEFORE we import our app, let's set an environmental variable
@@ -9,7 +8,7 @@ from datetime import datetime
 # before we import our app, since that will have already
 # connected to the database
 
-os.environ['DATABASE_URL'] = "postgresql:///warbler-test"
+os.environ['DATABASE_URL'] = "postgresql:///waddle-test"
 from app import app
 # Now we can import app
 # from seed import seed_data
@@ -25,10 +24,6 @@ class MessageModelTestCase(TestCase):
 
     def setUp(self):
         """Create test client, add sample data."""
-
-        User.query.delete()
-        Message.query.delete()
-        Follows.query.delete()
 
         self.client = app.test_client()
 
@@ -46,10 +41,19 @@ class MessageModelTestCase(TestCase):
                                user_id=u.id, timestamp=datetime.utcnow())
         db.session.add(test_message)
         db.session.commit()
-        
+
+    def tearDown(self):
+        """Clear sample data after each test."""
+
+        User.query.delete()
+        db.session.commit()
+        Message.query.delete()
+        db.session.commit()
+        Follows.query.delete()
+        db.session.commit()
 
     def test_message_model(self):
-        """Testing if basic message model works.""" 
+        """Testing if basic message model works."""
 
         self.assertEqual(Message.query.count(), 1)
 
@@ -65,8 +69,9 @@ class MessageModelTestCase(TestCase):
         test_query = Message.query.get_or_404(1)
         self.assertTrue(test_query.timestamp)
 
-    def test_user_id(self): 
-        """Testing to see if message's user's id is correct."""
+    def test_user_id(self):
+        """Testing to see if if id for person who posted the
+        message is correct."""
 
         test_query = Message.query.get_or_404(1)
         self.assertEqual(test_query.user_id, 1)
