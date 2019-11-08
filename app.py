b9e8523f654a9e71b04f63fccfe9038039eaa1d9
Kathy Nguyen <kathyn262@gmail.com>
2019-11-07 19:24:33 -0800

    added custom 404 page

--- app.py
+++ app.py
@@ -391,7 +391,12 @@ def homepage():
 
     else:
         return render_template('home-anon.html')
+   
 
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
 
 ##############################################################################
 # Turn off all caching in Flask
